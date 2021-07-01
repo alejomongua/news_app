c8c97c9fca64055029a21c8be64007c40940f9c6
Alejandro Mongua <alejandro.mongua@gmail.com>
2021-07-01 18:09:04 -0500

    Se estiliza la lista de categorías

--- lib/pages/second_tab_page.dart
+++ lib/pages/second_tab_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:news_provider/models/category_model.dart';
 import 'package:news_provider/services/news_service.dart';
 import 'package:provider/provider.dart';
 
@@ -32,19 +33,46 @@ class _CategoryList extends StatelessWidget {
         scrollDirection: Axis.horizontal,
         itemCount: categories.length,
         itemBuilder: (BuildContext context, int index) {
-          return Padding(
-            child: Column(
-              children: [
-                Icon(categories[index].icon),
-                SizedBox(
-                  height: 8,
+          final name =
+              '${categories[index].name[0].toUpperCase()}${categories[index].name.substring(1)}';
+          return GestureDetector(
+            onTap: () {},
+            child: Container(
+              width: 100,
+              child: Padding(
+                child: Column(
+                  children: [
+                    _CategoryIcon(categories[index]),
+                    SizedBox(
+                      height: 8,
+                    ),
+                    Text(name),
+                  ],
                 ),
-                Text(categories[index].name),
-              ],
+                padding: EdgeInsets.all(8),
+              ),
             ),
-            padding: EdgeInsets.all(8),
           );
         },
+      ),
+    );
+  }
+}
+
+class _CategoryIcon extends StatelessWidget {
+  final Category category;
+
+  const _CategoryIcon(this.category);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Icon(category.icon, color: Colors.black87),
+      width: 40,
+      height: 40,
+      decoration: BoxDecoration(
+        shape: BoxShape.circle,
+        color: Colors.white70,
       ),
     );
   }
